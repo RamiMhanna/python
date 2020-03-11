a283b4a3a9bec6d6f1b8f3eae712f36987de8cfc
Rami Mhanna <rami.mhanna@powercoders.org>
2020-03-11 02:54:48 +0100

    Using variable and importing data from the user

---
+++ py_01/app.py
@@ -0,0 +1,10 @@
+full_name = 'Rami Mhanna'
+age_01 = 33
+is_new = True
+
+name = input('Enter the Full name for the patient\n')
+print('Hi ' + name)
+color = input('Enter the favorite color of the patient\n')
+print(name + ' likes ' + color)
+age = input('Enter the age of the patient\n')
+print(name + "'s age is " + age)
