771f384d5f987516afaf2d018e3ad208ec5278b2
Rami Mhanna <rami.mhanna@powercoders.org>
2020-03-11 01:42:33 +0100

    Build a hello world to begin with Python

---
+++ hello_world/app.py
@@ -0,0 +1,19 @@
+print('Hello Rami')
+print('O_____')
+print(' ||||')
+print('*' * 10)
+
+
+price = 10
+print('price is : ', price)
+
+price = price * 3
+print('price is : ', price)
+
+price = price / 4
+print('price is : ', price)
+
+is_cheap = True
+
+if (is_cheap):
+    print('cheap')
