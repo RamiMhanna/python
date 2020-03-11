3ba887ad0e54e4100e27a1ac7b7e2ace32b646e9
Rami Mhanna <rami.mhanna@powercoders.org>
2020-03-11 07:35:20 +0100

    Using Strings and get used of '', "",""" """ and  ''' '''.

---
+++ string/app.py
@@ -0,0 +1,24 @@
+course = 'Python for Beginners'
+course_01 = "Python's course for Beginners"
+msg = """Python's course for Beginners
+
+Hi Rami, 
+This is your first Paragraph
+
+best regards 
+
+"""
+
+print(course)
+print(course_01)
+print(msg)
+
+print(course_01[2])
+print(course_01[-1])
+print(course_01[0:6])
+print(course_01[0:])
+print(course_01[:8])
+
+
+name = 'Jennifer'
+print(name[1:-1])
