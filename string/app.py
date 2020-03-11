34c6d4cd16a3bd6315a17864b976a67443f71e5f
Rami Mhanna <rami.mhanna@powercoders.org>
2020-03-11 08:39:49 +0100

    Using the several methods that are used with strings

--- string/app.py
+++ string/app.py
@@ -5,3 +5,20 @@ message = first + ' [' + last + '] is a coder'
 msg = f'{first} [{last}] is a coder'
 print(message)
 print(msg)
+
+course = 'Python for Beginners'
+
+print(len(course))
+course.upper()
+print(course.upper())
+print(course)
+print(course.lower())
+print(course.find('P'))
+print(course.find('o'))
+print(course.find('O'))
+print(course.replace('Beginners', 'Absolute Beginners'))
+print(course)
+
+print('Python' in course)
+print('python' in course)
+print(course.title())
