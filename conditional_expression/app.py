dafd1c9973602c0f6ee6c2e5d88f84d7e98d25dc
Rami Mhanna <rami.mhanna@powercoders.org>
2020-03-12 12:50:25 +0100

    Logical Operators

--- conditional_expression/app.py
+++ conditional_expression/app.py
@@ -1,6 +1,6 @@
 price = 1000000
 
-has_good_credit = False 
+has_good_credit = True
 
 if has_good_credit:
     down_payment = 0.1 * price
@@ -8,3 +8,13 @@ else:
     down_payment = 0.2 * price
 
 print(price - down_payment)
+###########################################
+has_high_income = True
+
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
+
+if has_high_income or has_good_credit:
+    print("Eligible for loan!!")
+
+
