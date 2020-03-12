2c0d441717740cc1bf46f9a05f3b6103cbeb4dd2
Rami Mhanna <rami.mhanna@powercoders.org>
2020-03-12 09:56:17 +0100

    Mathematical Operators

---
+++ py_02/app.py
@@ -0,0 +1,16 @@
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+x = 10
+x = x + 3
+print(x)
+x += 3
+print(x)
+
+y = (10 + 3) * 2 ** 2
+print(y)
