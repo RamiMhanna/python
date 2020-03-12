40577c3275ac44d41e0a67a4d6095a78c5ad08b4
Rami Mhanna <rami.mhanna@powercoders.org>
2020-03-12 10:03:48 +0100

    Math Module

--- py_02/app.py
+++ py_02/app.py
@@ -1,3 +1,5 @@
+import math
+
 print(10 + 3)
 print(10 - 3)
 print(10 * 3)
@@ -14,3 +16,10 @@ print(x)
 
 y = (10 + 3) * 2 ** 2
 print(y)
+
+x = 20.5
+print(round(x))
+print(abs(-x))
+print(math.ceil(x))
+print(math.floor(x))
+
