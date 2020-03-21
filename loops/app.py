a85a8111745e503bba861befb6942137d7c5011f
Rami Mhanna <rami.mhanna@powercoders.org>
2020-03-21 16:07:56 +0100

    Guess Game

--- loops/app.py
+++ loops/app.py
@@ -1,5 +1,13 @@
-i = 1
-while i <= 5:
-    print('*' * i)
-    i = i + 1
-print('Done')
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guessed = int(input("Guess:"))
+    guess_count += 1
+    if guessed == secret_number:
+        print("You Win !")
+        break
+# in python while loop has a else part which executed if the while loop finished without executing a break statement
+else:
+    print(f"your {guess_limit} tries is finished")
+
