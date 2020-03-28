2062cf9ba32a2d89304bd149df08360ad8b31457
Rami Mhanna <rami.mhanna@powercoders.org>
2020-03-28 05:42:25 +0100

    Emojies converter

--- dictionery/app.py
+++ dictionery/app.py
@@ -1,40 +1,10 @@
-persons = {
-    "name": "Rami",
-    "age": 30,
-    "phone": "5456468765",
-    "Email": "rami.mhanna@powercoders.org",
-    "is_verified": True
-}
-print(persons["age"])
-print(persons.get("age"))
-print(persons.get("Age"))
-print(persons.get("Age", "50"))
-
-persons["member"] = False
-print(persons)
-
-
-print("_" * 130)
-
-digits = {
-    "0": "ZERO",
-    "1": "ONE",
-    "2": "TWO",
-    "3": "THREE",
-    "4": "FOUR",
-    "5": "FIVE",
-    "6": "SIX",
-    "7": "SEVEN",
-    "8": "EIGHT",
-    "9": "NINE"
-}
+message = input("> ")
+msg = message.split(' ')
+emojis = {":)": "😊",
+          ":(": "😒"
+          }
 output = ""
-INPUT = input("Phone:")
-for digit in INPUT:
-    output += digits.get(digit)
-    output += "  "
+for word in msg:
 
-print(f"The written number is: {output}")
-
-
-print("_" * 130)
+    output += emojis.get(word, word) + " "
+print(output)
