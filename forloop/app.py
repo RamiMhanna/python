735294bd551e087d06050cf4e6ca77c8e97a7bbe
Rami Mhanna <rami.mhanna@powercoders.org>
2020-03-28 03:41:42 +0100

    List's methods

--- forloop/app.py
+++ forloop/app.py
@@ -1,20 +1,32 @@
-matrix = [[1, 2, 3],
-          [4, 5, 6],
-          [7, 8, 9]
-          ]
-print(matrix)
-x = matrix[0][2]
-print(x)
-print()
-for row in matrix:
-    for col in row:
-        print(col)
-print()
-list_of_numbers = [4, 6, 2, 8, 1, 9, 3]
-largest_number = 0
-for num in list_of_numbers:
-    if num > largest_number:
-        largest_number = num
-print(f"The largest number in the list is: {largest_number}")
+nums = [5, 4, 2, 1, 7, 4, 20]
+print(nums)
+nums_2 = nums.copy()
 
+nums.insert(2, 20)
+print(nums)
+nums.remove(7)
+print(nums)
+nums.pop()
+print(nums)
+print(nums.index(4))
+print(50 in nums)
+print(nums.count(4))
+nums.sort()
+print(nums)
+nums.reverse()
+print(nums)
+nums.clear()
+print(nums)
+print(nums_2)
+print("_" * 20)
 
+list_1 = [4, 6, 7, 2, 7, 4, 3, 6, 3, 30]
+list_2 = list_1.copy()
+print(list_2)
+for i in range(len(list_1)):
+    x = list_1[i]
+    for item in list_1[i+1:]:
+        if x == item:
+            print(x)
+            list_2.remove(item)
+print(list_2)
