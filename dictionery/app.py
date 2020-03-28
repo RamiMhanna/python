64535bd2773bf6c2f17020a5139796477d050b05
Rami Mhanna <rami.mhanna@powercoders.org>
2020-03-28 05:24:50 +0100

    Work with KEY and VALUE

--- dictionery/app.py
+++ dictionery/app.py
@@ -12,3 +12,26 @@ print(persons.get("Age", "50"))
 
 persons["member"] = False
 print(persons)
+
+
+print("_" * 130)
+
+digits = {
+    "0": "ZERO",
+    "1": "ONE",
+    "2": "TWO",
+    "3": "THREE",
+    "4": "FOUR",
+    "5": "FIVE",
+    "6": "SIX",
+    "7": "SEVEN",
+    "8": "EIGHT",
+    "9": "NINE"
+}
+output = ""
+INPUT = input("Phone:")
+for digit in INPUT:
+    output += digits[digit]
+    output += "  "
+
+print(f"The written number is: {output}")
