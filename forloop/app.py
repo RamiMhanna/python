281ee310c70c50f36b8b13c561acd82263c5371b
Rami Mhanna <rami.mhanna@powercoders.org>
2020-03-28 02:27:07 +0100

    Find the largest number in a list

--- forloop/app.py
+++ forloop/app.py
@@ -1,3 +1,6 @@
-numbers = [5, 2, 5, 2, 2]
-for num in numbers:
-    print("X" * num)
+list_of_numbers = [4, 6, 2, 8, 1, 9, 3]
+largest_number = 0
+for num in list_of_numbers:
+    if num > largest_number:
+        largest_number = num
+print(f"The largest number in the list is: {largest_number}")
