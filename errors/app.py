98c1a2c8fead1368ea342481b2e018749903ff6c
Rami Mhanna <rami.mhanna@powercoders.org>
2020-04-01 05:35:46 +0200

    Add comments to the code.

--- errors/app.py
+++ errors/app.py
@@ -3,7 +3,9 @@ try:
     income = 20000
     risk = income / age
     print(age)
+# Handling Division By Zero Error.
 except ZeroDivisionError:
     print("Age can not be 0")
+# Handling Invalid Input Error.
 except ValueError:
     print("Invalid Value")
