a649f1104ec8ba74cc08ef621bb9d70a5bbb74fb
Rami Mhanna <rami.mhanna@powercoders.org>
2020-05-03 06:13:19 +0200

    using string composition.

--- class_demo/demo.py
+++ class_demo/demo.py
@@ -4,6 +4,7 @@ connection = psycopg2.connect(dbname= "example", user="postgres", password= "111
 
 cursor = connection.cursor()
 
+cursor.execute('''DROP TABLE IF EXISTS rami_work;''')
 cursor.execute('''
     CREATE TABLE rami_work(
         id INTEGER PRIMARY KEY,
@@ -14,6 +15,14 @@ cursor.execute('''
 cursor.execute('''
     INSERT INTO rami_work(id, done) VALUES(1, true);
 ''')
+
+cursor.execute('''INSERT INTO rami_work(id, done) VALUES(%s, %s);''', (2, True))
+sql = '''INSERT INTO rami_work(id, done) VALUES(%(id)s, %(done)s);'''
+data = {
+    "id" : 3,
+    "done" : False
+}
+cursor.execute(sql, data)
 
 connection.commit()
 connection.close()
