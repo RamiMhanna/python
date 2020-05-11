3d8a9fdcd85154c62bf9a9b15be4cbfa0001db6a
Rami Mhanna <rami.mhanna@powercoders.org>
2020-05-11 23:52:28 +0200

    Fetch data from DataBase and apply it in the App.

--- sql_alchemy/hello_app/app.py
+++ sql_alchemy/hello_app/app.py
@@ -1,13 +1,21 @@
 from flask import Flask
-#from flask-sqlalchemy import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:111988@localhost:5432/example'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+  __tablename__ = 'persons'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(), nullable=False)
+
+db.create_all()
+
 @app.route('/')
 def index():
-    return "Hello World!!"
-
-if __name__ == '__main__':
-    app.run()
-
-    
+  person = Person.query.first()
+  return 'Hello ' + person.name
