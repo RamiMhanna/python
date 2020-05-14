0c52b1a8d8a2fa86b0f4ab2c6362d009c94d21d6
Rami Mhanna <rami.mhanna@powercoders.org>
2020-05-14 15:58:14 +0200

    Connect our todo app to database

--- sql_alchemy/crud_app/app.py
+++ sql_alchemy/crud_app/app.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:111988@localhost:5432/todoapp'
+
+class Todo(db.Model):
+    __tablename__ = 'todo'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    def __repr__(self):
+        return f"<todo {self.id} - {self.description}>"
+
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[
-        {
-        'description' : 'Todo 1'
-        }, 
-        {
-        'description' : 'Todo 2',
-        },
-        {
-        'description' : 'Todo 3'
-        }
-        ])
+    return render_template('index.html', data=Todo.query.all())
     
