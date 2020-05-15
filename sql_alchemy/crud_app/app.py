b9cc9c002dc6726191d30e884905feb80078703f
Rami Mhanna <rami.mhanna@powercoders.org>
2020-05-16 00:55:15 +0200

    Add a Migration File and Migration Upgrade to create the table todo and its Columns in effective way

--- sql_alchemy/crud_app/app.py
+++ sql_alchemy/crud_app/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys 
 
 app = Flask(__name__)
 #db = SQLAlchemy(app, session_options={"expire_on_commit": False})
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:111988@localhost:5432/todoapp'
 
@@ -12,11 +15,10 @@ class Todo(db.Model):
     __tablename__ = 'todo'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    
     def __repr__(self):
         return f"<todo {self.id} - {self.description}>"
-
-
-db.create_all()
 
 @app.route('/todo/create', methods=['POST'])
 def create_todo():
